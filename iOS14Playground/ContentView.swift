640e9c4bdf4a590c189dda41a889a3a7492cf61a
snakagam <snakagam@yahoo-corp.jp>
2020-07-13 10:23:17 +0900

    Initial Commit

---
+++ iOS14Playground/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  iOS14Playground
+//
+//  Created by Shota Nakagami on 2020/07/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOS14Playground/iOS14PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS14PlaygroundApp.swift
+//  iOS14Playground
+//
+//  Created by Shota Nakagami on 2020/07/13.
+//
+
+import SwiftUI
+
+@main
+struct iOS14PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
